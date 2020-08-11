323bb08ab783599477cc46b61509a40626ef88a2
Manoel Malafaia <mmalafaia@gmail.com>
2020-08-11 17:17:41 -0300

    Small adjustes

--- mobile/src/components/Button/styles.ts
+++ mobile/src/components/Button/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
-  width: 100%;
+  width: 330px; /*  deveria ser 100% */
   height: 60px;
   background: #ff9000;
   border-radius: 10px;
@@ -13,6 +13,7 @@ export const Container = styled(RectButton)`
 `;
 
 export const ButtonText = styled.Text`
+  /* width: 100%; */
   font-family: 'RobotoSlab-Medium';
   color: #312e38;
   font-size: 18px;
