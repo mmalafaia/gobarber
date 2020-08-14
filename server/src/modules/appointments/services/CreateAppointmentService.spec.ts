a435162877b3cf63d2587cb625943c83bc883209
Manoel Malafaia <mmalafaia@gmail.com>
2020-08-14 18:04:25 -0300

    Appointment Tests

--- server/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
@@ -17,7 +19,23 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123123');
   });
 
-  // it('shold not be abble to create two appointments in the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('shold not be abble to create two appointments in the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
