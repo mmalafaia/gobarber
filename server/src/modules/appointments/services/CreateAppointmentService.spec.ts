03524872651a706a2ddcd8b7a71980165f39e7fb
Manoel Malafaia <mmalafaia@gmail.com>
2020-08-14 17:42:53 -0300

    First unity test

---
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,23 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('shold be abble to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+
+  // it('shold not be abble to create two appointments in the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
