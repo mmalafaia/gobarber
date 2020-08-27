0635a63e653dc769c37a4f92b498fa0cbd92b72a
Manoel Malafaia <mmalafaia@gmail.com>
2020-08-27 13:25:13 -0300

    List Available days

--- server/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ server/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,7 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Raw } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
@@ -17,6 +18,26 @@ class AppointmensRepository implements IAppointmentsRepository {
       where: { date },
     });
     return findAppointment;
+  }
+
+  public async findByAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const parsedMonth = String(month).padStart(2, '0');
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateFildName =>
+            `to_char(${dateFildName}, 'MM-YYYY') = '${parsedMonth}=${year}'`,
+        ),
+      },
+    });
+
+    return appointments;
   }
 
   public async create({
---
+++ server/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,60 @@
+import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  year: number;
+  month: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findByAllInMonthFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    // cria um array com a quantidade de dias do mês
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
