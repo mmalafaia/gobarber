6d7ca38b24efe2ae0097c850660ef21fc7d8374f
Manoel Malafaia <mmalafaia@gmail.com>
2020-09-01 16:44:34 -0300

    Cache completed

--- server/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ server/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -27,20 +27,26 @@ class ListProviderAppointmentsService {
     month,
     day,
   }: IRequest): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('asd');
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    console.log(cacheData);
-
-    const appointments = await this.appointmentsRepository.findByAllInDayFromProvider(
-      {
-        provider_id,
-        year,
-        month,
-        day,
-      },
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
     );
 
-    // await this.cacheProvider.save('asd', 'asdasda');
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findByAllInDayFromProvider(
+        {
+          provider_id,
+          year,
+          month,
+          day,
+        },
+      );
+
+      console.log('Buscou do Banco');
+
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
     return appointments;
   }
