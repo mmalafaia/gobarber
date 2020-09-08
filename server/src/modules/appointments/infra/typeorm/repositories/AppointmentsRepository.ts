192ebe607fd08250dc004eb55095e26c68eab18f
Manoel Malafaia <mmalafaia@gmail.com>
2020-09-08 10:09:39 -0300

    server adjusts

--- server/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ server/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -14,9 +14,12 @@ class AppointmensRepository implements IAppointmentsRepository {
     this.ormRepository = getRepository(Appointment);
   }
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {
+  public async findByDate(
+    date: Date,
+    provider_id: string,
+  ): Promise<Appointment | undefined> {
     const findAppointment = await this.ormRepository.findOne({
-      where: { date },
+      where: { date, provider_id },
     });
     return findAppointment;
   }
@@ -58,6 +61,7 @@ class AppointmensRepository implements IAppointmentsRepository {
             `to_char(${dateFildName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
