fd8ef087ea948b1cb117656ceffb3ac4f6aea985
Manoel Malafaia <mmalafaia@gmail.com>
2020-09-14 12:08:00 -0300

    Dashboard

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -4,6 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
 
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
+import Profile from '../pages/Profile';
+
 const App = createStackNavigator();
 
 const AuthRoutes: React.FC = () => (
@@ -14,6 +18,10 @@ const AuthRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
