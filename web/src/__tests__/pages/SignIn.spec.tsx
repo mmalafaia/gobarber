f9a498dd49cde8ffa77bce29147c852d004500b6
Manoel Malafaia <mmalafaia@gmail.com>
2020-09-17 16:47:20 -0300

    Settings Tests

---
+++ web/src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import SignIn from '../../pages/SignIn';
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: jest.fn(),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+describe('SignIn Page', () => {
+  it('shold be abble to sign in', () => {
+    const { debug } = render(<SignIn />);
+
+    debug();
+  });
+});
